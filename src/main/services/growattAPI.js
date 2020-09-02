d691b30f848f8958360f92323912264bbe9b79d2
DuilioML <mauro.duilio@mercadolibre.com>
2020-09-02 00:32:15 -0300

    using cookies with calls

--- src/main/services/growattAPI.js
+++ src/main/services/growattAPI.js
@@ -1,18 +1,59 @@
 var request = require('request');
 
-async function CallLogin() {
+async function CallLogin(user, pass) {
   return new Promise ((resolve, reject) => {
     request({
       method: 'POST',
       uri: 'http://server.growatt.com/LoginAPI.do?',
       qs: {
-          userName: 'Matias Toledo',
-          password: '57c93b317857a3437f3b778d9455529a'
+        userName: user,
+        password: pass
+      }
+    }, (err, res, body) => {
+      resolve(res);
+    });
+  });
+}
+
+function GetPlants(userId, headers) {
+  return new Promise ((resolve, reject) => {
+    request({
+      method: 'GET',
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "cookie": headers
+      },
+      uri: 'http://server.growatt.com/PlantListAPI.do?',
+      qs: {
+        userId: userId
       }
     }, (err, res, body) => {
       resolve(JSON.parse(body));
     });
+  });
+}
+
+function GetPlantDetial(plantId, cookies) {
+  return new Promise ((resolve, reject) => {
+    request({
+      method: 'GET',
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "cookie": cookies
+      },
+      uri: 'http://server.growatt.com/newPlantAPI.do?',
+      qs: {
+        op: "getAllDeviceListThree",
+        plantId: plantId,
+        pageNum: 1,
+        pageSize: 1
+      }
+    }, (err, res, body) => {
+      resolve(JSON.parse(body));
     });
-  }
- 
-module.exports = CallLogin;
+  });
+}
+
+module.exports = { CallLogin, GetPlantDetial, GetPlants};
